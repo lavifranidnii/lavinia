30819ba2e237aa89627d4a0bf08c53d879589652
lavifranidnii <148551556+lavifranidnii@users.noreply.github.com>
2024-09-03 11:13:59 -0300

    llllllll

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-const elemento = document. querySelector("teste");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".textoresultado");
+
+
+const perguntas = [
+    {
+            enunciado"qual a data da independencia do brasil"
+             resposta
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  Function mostraPergunta ()
+  perguntaAtual =perguntas [atual];
+ 
